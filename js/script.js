e5ce7a9af93591804efdb233b49ce4271aadc90b
francopirol <pirolofip@gmail.com>
2022-04-26 21:01:05 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let dogChow = 700;
+let royalCanin = 1500;
+let voraz = 500;
+let pedigree = 1300;
+let oldPrince = 2000;
+
+let catChow = 700;
+let whiskas = 1500;
+let vorazCat = 500;
+let royalKitten = 1300;
+let felinnes = 2000;
